133184e0fee4f04a6291bffb67e48a8d2e229b93
r0b0tAstronaut <nickwcalifano@gmail.com>
2025-02-01 17:40:49 -0500

    converted llm utils to a class

--- llm_utils.py
+++ llm_utils.py
@@ -1,163 +1,158 @@
 """ This module has functions to call Ollama and Azure OpenAI endpoints """
-import time
-from typing import Literal, Union
-import os
+import asyncio
 import json
+import os
+from typing import Literal, Union
+
 import aiohttp
 import numpy as np
 
-# Define GPT endpoints and retry parameters
-URL_AZURE = "https://{endpoint}/openai/deployments/{deployment-id}/chat/completions?api-version=2024-10-21"
-AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
+# Constants for retry logic
 NUM_REQUEST_RETRIES = 10 # If we have hit our token quota, we wait some time then try again (exponential backoff)
 MAX_WAIT_TIME = 60 # Max time in seconds to sleep each time we wait
-AZURE_HEADERS = {
-    "Content-Type": "application/json",
-    "api-key": AZURE_OPENAI_KEY
-}
 
-# Define ollama endpoints
-URL_OLLAMA = "http://localhost:11434/api/generate"
-ALLOWED_MODEL_STRINGS = Literal["deepseek-r1:14b", "llama3.2-vision", "phi4", "gpt4o-mini", "gpt4o"]
-SERVER_TYPES = Literal["azure_openai", "ollama"]
+# Type aliases
+AllowedModelNames  = Literal["deepseek-r1:14b", "llama3.2-vision", "phi4", "gpt4o-mini", "gpt4o"]
+ServerTypes = Literal["azure_openai", "ollama"]
 
-def get_model_type(model_name: ALLOWED_MODEL_STRINGS) -> SERVER_TYPES:
-    """ Returns llama if the model is a llama model and returns gpt if the model is a gpt model
-    """
-    if "gpt" in model_name:
-        return "azure_openai"
-    return "ollama"
+class AzureOpenAiRetriesExhaustedError(Exception):
+    """ Azure OpenAI Service retries have been exhausted."""
 
+class LlmUtils():
+    """ This class has utility functions to call Ollama and Azure OpenAI endpoints """
 
-async def call_cached_llm(
-        model_name: ALLOWED_MODEL_STRINGS,
+    # Configuration from environment variables (checked at startup)
+    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
+    AZURE_OPENAI_URL = os.environ.get("AZURE_OPENAI_URL")
+    OLLAMA_URL = os.environ.get("OLLAMA_URL")
+
+    if not all([AZURE_OPENAI_KEY, AZURE_OPENAI_URL, OLLAMA_URL]):
+        print(AZURE_OPENAI_KEY, AZURE_OPENAI_URL, OLLAMA_URL)
+        missing_vars = [var for var, val in
+                        {"AZURE_OPENAI_KEY": AZURE_OPENAI_KEY,
+                         "AZURE_OPENAI_URL": AZURE_OPENAI_URL,
+                         "OLLAMA_URL": OLLAMA_URL}.items() if val is None]
+        raise ValueError(f"Missing environment variables: {', '.join(missing_vars)}")
+
+    @staticmethod
+    def get_model_type(model_name: AllowedModelNames) -> ServerTypes:
+        """Returns the server type based on the model name."""
+        return "azure_openai" if "gpt" in model_name else  "ollama"
+
+    @staticmethod
+    def create_hashable_payload(model_name, request_payload):
+        """Creates a hashable version of the request payload for caching."""
+        model_type = LlmUtils.get_model_type(model_name)
+        if model_type == "ollama":
+            return (
+                model_name,
+                request_payload["prompt"],
+                tuple(tuple(option) for option in request_payload["options"].items())
+            )
+
+        # Azure OpenAI
+        return (
+            model_name,
+            request_payload["messages"][0]["content"], request_payload["messages"][1]["content"],
+            tuple(request_payload[item] for item in ["temperature", "top_p"])
+        )
+
+    @classmethod
+    async def call_cached_llm(
+            cls,
+            model_name: AllowedModelNames,
+            session: aiohttp.ClientSession,
+            request_payload: dict,
+            cache: dict,
+            returned_stopped_only: bool=True
+        ) -> Union[str, None]:
+        """Wrapper around call_llm. Uses cached result if available. Saves new result to cache."""
+        hashable_payload = cls.create_hashable_payload(model_name, request_payload)
+
+        if hashable_payload in cache:
+            return cache[hashable_payload]
+
+        result = await cls.call_llm(model_name, session, request_payload, returned_stopped_only)
+        if result is not None:
+            cache[hashable_payload] = result
+        return result
+
+    @classmethod
+    async def call_llm(
+            cls,
+            model_name: AllowedModelNames,
+            session: aiohttp.ClientSession,
+            request_payload: dict,
+            returned_stopped_only: bool=True
+        ) -> Union[str, None]:
+        """Calls the given model's generate endpoint."""
+        model_type = cls.get_model_type(model_name)
+        handler = cls.call_ollama if model_type == "ollama" else cls.call_azure_openai
+        return await handler(session, request_payload, returned_stopped_only)
+
+    @classmethod
+    async def call_ollama(
+        cls,
         session: aiohttp.ClientSession,
         request_payload: dict,
-        cache: dict,
         returned_stopped_only: bool=True
     ) -> Union[str, None]:
-    """ Wrapper around call_llm. Uses cached result if available, and saves result to cache if it wasn't available
-    """
-    # Get the model type (ollama or GPT)
-    model_type = get_model_type(model_name)
-
-    # Get a hashable version of the payload - we will be caching the results in a dict
-    if model_type == "ollama":
-        hashable_payload = tuple(
-            [model_name, request_payload["prompt"]] +
-            [tuple(option) for option in request_payload["options"].items()]
-        )
-    else:
-        hashable_payload = tuple(
-            [model_name, request_payload["messages"][0]["content"], request_payload["messages"][1]["content"]] +
-            [request_payload[item] for item in ["temperature", "top_p"]]
-        )
-
-    # If this request has already been sent, use the cached version
-    if hashable_payload in cache:
-        return cache[hashable_payload]
-
-    # Since the request is new, make a call to the LLM and save the result in the cache
-    result = await call_llm(model_name, session, request_payload, returned_stopped_only)
-
-    if result is not None:
-        cache[hashable_payload] = result
-
-    return result
-
-
-async def call_llm(
-        model_name: ALLOWED_MODEL_STRINGS,
-        session: aiohttp.ClientSession,
-        request_payload: dict,
-        returned_stopped_only: bool=True
-    ) -> Union[str, None]:
-    """ Calls the given model's generate endpoint using the given request payload
-    """
-    # Get the model type (ollama or GPT)
-    model_type = get_model_type(model_name)
-
-    # Llama and Azure OpenAI have slightly different APIs, so we need to call them slightly differently
-    if model_type == "ollama":
-        return await call_ollama(session, request_payload, returned_stopped_only)
-    return await call_azure_openai(session, request_payload, returned_stopped_only)
-
-
-async def call_ollama(
-    session: aiohttp.ClientSession,
-    request_payload: dict,
-    returned_stopped_only: bool=True
-) -> Union[str, None]:
-    """ Calls an ollama generate endpoint using the given request payload
-    """
-    try:
-        async with session.post(URL_OLLAMA, json=request_payload) as resp:
-            response = await resp.json()
-            resp.raise_for_status()
-
-        # If the model hits the output token limit, simply return None
-        if returned_stopped_only and response["done_reason"] != "stop":
-            retval = None
-        else:
-            retval = response["response"]
-
-    except aiohttp.ClientResponseError as e:
-        print(
-            "LLAMA ClientResponseError", e,
-            "\nRequest:", request_payload,
-            "\nResponse:", json.dumps(response)
-        )
-        return None
-
-    return retval
-
-
-async def call_azure_openai(
-    session: aiohttp.ClientSession,
-    request_payload: dict,
-    returned_stopped_only: bool=True
-) -> Union[str, None]:
-    """ Calls an Azure OpenAI generate endpoint using the given request payload
-    """
-    for i in range(NUM_REQUEST_RETRIES):
+        """Calls an Ollama generate endpoint."""
         try:
-            async with session.post(URL_AZURE, headers=AZURE_HEADERS, json=request_payload) as resp:
+            async with session.post(cls.OLLAMA_URL, json=request_payload) as resp:
                 response = await resp.json()
                 resp.raise_for_status()
 
             # If the model hits the output token limit, simply return None
-            if returned_stopped_only and response["choices"][0]["finish_reason"] != "stop":
-                retval = None
-            else:
-                retval = response["choices"][0]["messages"][0]["content"]
-
-            # If the model returns a valid response, there's no need for further retries. Break out of the for-loop
-            break
+            if returned_stopped_only and response["done_reason"] != "stop":
+                return None
+            return response["response"]
 
         except aiohttp.ClientResponseError as e:
-            # 400 is for ill-formatted request payload as well as for content filter flags
-            if resp.status == 400:
-                print(
-                    "GPT ClientResponseError", e,
-                    "\nRequest:", request_payload,
-                    "\nResponse:", json.dumps(response)
-                )
-                retval = None
-                break
+            print(f"LLAMA ClientResponseError {e}\nRequest: {request_payload}\nResponse: {json.dumps(response)}")
+            return None
+        except Exception as e:
+            print(f"Ollama Error: {e}\nRequest: {request_payload}")
+            return None
 
-            # 429 is for quote rate limit reached. If the request failed due to quota, wait then try again
-            # Similarly, if it is a 500 error, wait then try again to see if it fixes itself
-            if resp.status == 429 or 500 <= resp.status < 600:
-                time.sleep(min(MAX_WAIT_TIME, np.power(2, i)))
+    @classmethod
+    async def call_azure_openai(
+        cls,
+        session: aiohttp.ClientSession,
+        request_payload: dict,
+        returned_stopped_only: bool=True
+    ) -> Union[str, None]:
+        """Calls an Azure OpenAI generate endpoint."""
+        headers = {
+            "Content-Type": "application/json",
+            "api-key": cls.AZURE_OPENAI_KEY,
+        }
+        for i in range(NUM_REQUEST_RETRIES):
+            try:
+                async with session.post(cls.AZURE_OPENAI_URL, headers=headers, json=request_payload) as resp:
+                    response = await resp.json()
+                    resp.raise_for_status()
 
-            # If the request failed due to any other reason: print the request and response then raise an error
-            # If this was production code, I would have better error handling
-            else:
-                print(
-                    "GPT ClientResponseError", e,
-                    "\nRequest:", request_payload,
-                    "\nResponse:", json.dumps(response)
-                )
-                raise e
+                # If the model hits the output token limit, simply return None
+                if returned_stopped_only and response["choices"][0]["finish_reason"] != "stop":
+                    return None
+                return response["choices"][0]["message"]["content"]
 
-    return retval
+            except aiohttp.ClientResponseError as e:
+                # 400 is for ill-formatted request payload as well as for content filter flags
+                if resp.status == 400:
+                    print("GPT ClientResponseError {e}\nRequest: {request_payload}\nResponse: {json.dumps(response)}")
+                    return None
+
+                # 429 is for quote rate limit reached. If the request failed due to quota, wait then try again
+                # Similarly, if it is a 500 error, wait then try again to see if it fixes itself
+                if resp.status == 429 or 500 <= resp.status < 600:
+                    await asyncio.sleep(min(MAX_WAIT_TIME, np.power(2, i)))
+
+                # If the request failed due to any other reason: print the request and response then raise an error
+                # If this was production code, I would have better error handling
+                else:
+                    print("GPT ClientResponseError {e}\nRequest: {request_payload}\nResponse: {json.dumps(response)}")
+                    raise e
+
+        raise AzureOpenAiRetriesExhaustedError("Max retries exceeded for Azure OpenAI")
